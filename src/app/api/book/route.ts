83e00e5d272a6e889da9e05d5001f9528da4649b
SuCicada <pengyifu@gmail.com>
2024-06-22 19:05:46 +0900

    api book: force-dynamic

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -3,6 +3,7 @@ import {NextResponse} from "next/server";
 import {PrismaClient} from "@prisma/client";
 import * as Prisma from "@prisma/client";
 import {sql} from "@/app/utils/db";
+export const dynamic = "force-dynamic";
 
 // export const dynamic = 'force-dynamic'
 const prisma = new PrismaClient();
